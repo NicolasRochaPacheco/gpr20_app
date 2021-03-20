ea52391015c81a01e06d922b3ce9c5129561fe95
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2021-03-20 10:59:35 -0500

    Fixed links to modules

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
         />
 
         <Switch>
-          <Route path="/gpr20_app/" exact>
+          <Route path="/" exact>
             <Home 
               status={this.state.rosConnected}
               ip={this.state.ip}
