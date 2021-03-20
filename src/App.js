5682d1c7db624f8d670be79c6e53ed6a289ddba1
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2021-03-20 10:33:45 -0500

    Modified App to work on GitHub

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
         />
 
         <Switch>
-          <Route path="/" exact>
+          <Route path="/gpr20_app/" exact>
             <Home 
               status={this.state.rosConnected}
               ip={this.state.ip}
@@ -102,8 +102,8 @@ class App extends React.Component {
             />
           </Route>
 
-          <Route path="/survey" component={Survey} />
-          <Route path="/data" component={Data} />
+          <Route path="/gpr20_app/survey" component={Survey} />
+          <Route path="/gpr20_app/data" component={Data} />
         </Switch>
       </Router>
     );
